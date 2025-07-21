f0c02d16f595ccd2b10eda6c5fac59617cad7be7
Tarik Pinarli <tpinarli@c3r4p1.hive.fi>
2025-07-21 16:41:47 +0300

    wall logic implemented

--- src/init_dummy_data/init_dummy_data.c
+++ src/init_dummy_data/init_dummy_data.c
@@ -6,7 +6,7 @@
 /*   By: tpinarli <tpinarli@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 13:44:38 by tpinarli          #+#    #+#             */
-/*   Updated: 2025/07/16 15:47:08 by tpinarli         ###   ########.fr       */
+/*   Updated: 2025/07/21 14:36:34 by tpinarli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,42 +16,42 @@
 #include <player.h>
 #include <colors.h>
 
-// void load_textures(t_game *game)
-// {
-//     // Load texture files
-//     game->textures->north_png = mlx_load_png(game->textures->north_path);
-//     if (!game->textures->north_png)
-//         error_exit("Failed to load north texture", game);
+void load_textures(t_game *game)
+{
+    // Load texture files
+    game->textures->north_png = mlx_load_png(game->textures->north_path);
+    if (!game->textures->north_png)
+        error_exit("Failed to load north texture", game);
 
-//     game->textures->south_png = mlx_load_png(game->textures->south_path);
-//     if (!game->textures->south_png)
-//         error_exit("Failed to load south texture", game);
+    game->textures->south_png = mlx_load_png(game->textures->south_path);
+    if (!game->textures->south_png)
+        error_exit("Failed to load south texture", game);
 
-//     game->textures->east_png = mlx_load_png(game->textures->east_path);
-//     if (!game->textures->east_png)
-//         error_exit("Failed to load east texture", game);
+    game->textures->east_png = mlx_load_png(game->textures->east_path);
+    if (!game->textures->east_png)
+        error_exit("Failed to load east texture", game);
 
-//     game->textures->west_png = mlx_load_png(game->textures->west_path);
-//     if (!game->textures->west_png)
-//         error_exit("Failed to load west texture", game);
+    game->textures->west_png = mlx_load_png(game->textures->west_path);
+    if (!game->textures->west_png)
+        error_exit("Failed to load west texture", game);
 
-//     // Convert textures to images
-//     game->textures->north_img = mlx_texture_to_image(game->mlx, game->textures->north_png);
-//     if (!game->textures->north_img)
-//         error_exit("Failed to convert north texture to image", game);
+    // Convert textures to images
+    // game->textures->north_img = mlx_texture_to_image(game->mlx, game->textures->north_png);
+    // if (!game->textures->north_img)
+    //     error_exit("Failed to convert north texture to image", game);
 
-//     game->textures->south_img = mlx_texture_to_image(game->mlx, game->textures->south_png);
-//     if (!game->textures->south_img)
-//         error_exit("Failed to convert south texture to image", game);
+    // game->textures->south_img = mlx_texture_to_image(game->mlx, game->textures->south_png);
+    // if (!game->textures->south_img)
+    //     error_exit("Failed to convert south texture to image", game);
 
-//     game->textures->east_img = mlx_texture_to_image(game->mlx, game->textures->east_png);
-//     if (!game->textures->east_img)
-//         error_exit("Failed to convert east texture to image", game);
+    // game->textures->east_img = mlx_texture_to_image(game->mlx, game->textures->east_png);
+    // if (!game->textures->east_img)
+    //     error_exit("Failed to convert east texture to image", game);
 
-//     game->textures->west_img = mlx_texture_to_image(game->mlx, game->textures->west_png);
-//     if (!game->textures->west_img)
-//         error_exit("Failed to convert west texture to image", game);
-// }
+    // game->textures->west_img = mlx_texture_to_image(game->mlx, game->textures->west_png);
+    // if (!game->textures->west_img)
+    //     error_exit("Failed to convert west texture to image", game);
+}
 
 static t_map *init_dummy_map(t_game *game)
 {
@@ -99,13 +99,13 @@ static t_textures *init_dummy_textures(t_game *game)
 	{
 		error_exit("Failed to allocate dummy textures", game);
 	}
-	textures->north_path = arena_strdup(game->arena, "textures/wall_2.png");
+	textures->north_path = arena_strdup(game->arena, "textures/wall.png");
 	if (!textures->north_path)
 		error_exit("textures->north initialization failed", game);
 	textures->south_path = arena_strdup(game->arena, "textures/wall_2.png");
 	if (!textures->south_path)
 		error_exit("textures->south initialization failed", game);
-	textures->west_path = arena_strdup(game->arena, "textures/wall_2.png");
+	textures->west_path = arena_strdup(game->arena, "textures/wall.png");
 	if (!textures->west_path)
 		error_exit("textures->west initialization failed", game);
 	textures->east_path = arena_strdup(game->arena, "textures/wall_2.png");
@@ -147,7 +147,7 @@ int init_dummy_data(t_game *game)
 {
     game->map = init_dummy_map(game);
     game->textures = init_dummy_textures(game);
-    //load_textures(game);
+    load_textures(game);
     game->player = init_dummy_player(game);
 
     init_dummy_colors(&game->floor, &game->ceiling);
