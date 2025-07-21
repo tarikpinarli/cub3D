92c72135f30d49ce78ab84902935029188bc090c
Tarik Pinarli <tpinarli@c3r4p1.hive.fi>
2025-07-21 12:39:09 +0300

    arana_substr added to /arena

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: tpinarli <tpinarli@student.hive.fi>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/07/07 13:16:45 by tpinarli          #+#    #+#              #
-#    Updated: 2025/07/21 12:21:43 by tpinarli         ###   ########.fr        #
+#    Updated: 2025/07/21 12:35:42 by tpinarli         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -30,6 +30,7 @@ SRCS        =	main.c error_exit.c \
 				arena/arena.c \
 				arena/arena_strdup.c \
 				arena/arena_split.c \
+				arena/arena_substr.c \
 				game/mlx_init.c \
 				cleanup/close_window.c \
 				handle_keypress/handle_keypress.c \
--- src/arena/arena_substr.c
+++ src/arena/arena_substr.c
@@ -1,34 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   arena.h                                            :+:      :+:    :+:   */
+/*   arena_substr.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tpinarli <tpinarli@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/07 15:20:00 by tpinarli          #+#    #+#             */
-/*   Updated: 2025/07/21 12:35:25 by tpinarli         ###   ########.fr       */
+/*   Created: 2025/07/21 12:34:28 by tpinarli          #+#    #+#             */
+/*   Updated: 2025/07/21 12:38:28 by tpinarli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ARENA_H
-#define ARENA_H
-
 #include <cub3d.h>
-# include <stdio.h>
-#include <stdlib.h>
 
-typedef struct s_arena
-{
-    void    *memory;
-    size_t   size;
-    size_t   offset;
-}   t_arena;
-
-t_arena *arena_init(size_t size);
-void 	*arena_alloc(t_arena *arena, size_t size);
-void 	arena_destroy(t_arena *arena);
-char 	*arena_strdup(t_arena *arena, const char *s);
-char	**arena_split(t_arena *arena, char const *s, char c);
 char	*arena_substr(t_arena *arena, const char *s,
-		unsigned int start, size_t len);
-#endif
+		unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	len1;
+	size_t	s_len;
+	char	*res;
+
+	if (!s || !arena)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		len1 = 0;
+	else if (len > s_len - start)
+		len1 = s_len - start;
+	else
+		len1 = len;
+	res = (char *)arena_alloc(arena, (len1 + 1) * sizeof(char));
+	if (!res)
+		return (NULL); // Arena allocatiion fail.
+	i = 0;
+	while (i < len1)
+	{
+		res[i] = s[start + i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
