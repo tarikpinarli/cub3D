82764d7cf3ce775656846814cd2bfaa11feab8e7
Tarik Pinarli <tpinarli@c2r4p6.hive.fi>
2025-07-26 13:05:04 +0300

    deleted replica files

--- include/colors.h
+++ include/colors.h
@@ -20,4 +20,11 @@ typedef struct s_color {
 	int	b;
 }	t_color;
 
+typedef struct s_pixel_color {
+	uint8_t	r;
+	uint8_t	g;
+	uint8_t	b;
+	uint8_t	a;
+}	t_pixel_color;
+
 #endif
--- src/handle_keypress/update_player.c
+++ src/handle_keypress/update_player.c
@@ -6,31 +6,17 @@
 /*   By: tpinarli <tpinarli@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 13:44:38 by tpinarli          #+#    #+#             */
-/*   Updated: 2025/07/16 16:31:27 by tpinarli         ###   ########.fr       */
+/*   Updated: 2025/07/26 13:02:50 by tpinarli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
 #define MOVE_SPEED 0.035
-#define ROT_SPEED 0.025
+#define ROT_SPEED 0.020
 #define HOR_MOVE 0.019
 
 void	move_forward(t_game *game)
-{
-	double next_x;
-	double next_y;
-
-	next_x = game->player->x - cos(game->player->dir) * MOVE_SPEED;
-	next_y = game->player->y - sin(game->player->dir) * MOVE_SPEED;
-	if (game->map->grid[(int)next_y][(int)next_x] != '1')
-	{
-		game->player->x = next_x;
-		game->player->y = next_y;
-	}
-}
-
-void	move_backward(t_game *game)
 {
 	double next_x;
 	double next_y;
@@ -44,11 +30,25 @@ void	move_backward(t_game *game)
 	}
 }
 
+void	move_backward(t_game *game)
+{
+	double next_x;
+	double next_y;
+
+	next_x = game->player->x - cos(game->player->dir) * MOVE_SPEED;
+	next_y = game->player->y - sin(game->player->dir) * MOVE_SPEED;
+	if (game->map->grid[(int)next_y][(int)next_x] != '1')
+	{
+		game->player->x = next_x;
+		game->player->y = next_y;
+	}
+}
+
 void	move_left(t_game *game)
 {
 	double	dir = game->player->dir;
-	double	next_x = game->player->x - sin(dir) * HOR_MOVE;
-	double	next_y = game->player->y + cos(dir) * HOR_MOVE;
+	double	next_x = game->player->x + sin(dir) * HOR_MOVE;
+	double	next_y = game->player->y - cos(dir) * HOR_MOVE;
 
 	if (game->map->grid[(int)next_y][(int)next_x] != '1')
 	{
@@ -60,8 +60,8 @@ void	move_left(t_game *game)
 void	move_right(t_game *game)
 {
 	double	dir = game->player->dir;
-	double	next_x = game->player->x + sin(dir) * HOR_MOVE;
-	double	next_y = game->player->y - cos(dir) * HOR_MOVE;
+	double	next_x = game->player->x - sin(dir) * HOR_MOVE;
+	double	next_y = game->player->y + cos(dir) * HOR_MOVE;
 
 	if (game->map->grid[(int)next_y][(int)next_x] != '1')
 	{
@@ -105,6 +105,4 @@ void	update_player(t_game *game)
 		rotate_left(game);
 	if (mlx_is_key_down(game->mlx, MLX_KEY_RIGHT))
 		rotate_right(game);
-	if (mlx_is_key_down(game->mlx, MLX_KEY_SPACE) && !game->is_jumping)
-		game->is_jumping = true;
 }
